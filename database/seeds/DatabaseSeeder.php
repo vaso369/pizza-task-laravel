b0492dea17acccedc37ca7eb3a9e5b72c0582712
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-07-23 16:25:28 +0200

    user_role_model_created_and_database_seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('roles')->insert([
+            [
+                'role_name' => 'admin'
+            ],
+            [
+                'role_name' => 'user'
+            ]
+        ]);
+        $faker = Faker::create();
+    	foreach (range(1,10) as $index) {
+	        DB::table('users')->insert([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+	            'email' => $faker->email,
+                'password' => bcrypt('secret'),
+                'is_active' => 1,
+                'role_id' => 2
+
+            ]);
+            }
     }
 }
