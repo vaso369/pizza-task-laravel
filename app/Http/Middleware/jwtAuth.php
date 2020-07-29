c8b2627df3524d3db4f25a202516f3f81e1a80b0
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-07-29 13:24:06 +0200

    order changes

--- app/Http/Middleware/jwtAuth.php
+++ app/Http/Middleware/jwtAuth.php
@@ -33,7 +33,7 @@ class jwtAuth
                     $decoded = JWT::decode($jwt, $secret_key, array('HS256'));
                    // $request->id=$decoded->user->id;
                     // Access is granted.
-                    return response(['asdasd'=>'asdasd']);
+                    return response(['asdasd'=>$decoded]);
                     return $next($request);
 
                 } catch (\Exception $e) {
