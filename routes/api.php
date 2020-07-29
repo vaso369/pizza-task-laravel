b6fdacc038a7cd386f296c5602c3c9c9de0f184c
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-07-29 12:05:25 +0200

    order changes

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('/user','UserController@register');
 Route::post('/login','UserController@login');
 Route::post('/order', 'OrderController@makeOrder');
 Route::group(['middleware' => 'jwtAuth'], function () {
-    Route::get('/order', 'OrderController@getUserOrders');
+    Route::get('/orders', 'OrderController@getUserOrders');
 
 });
 
